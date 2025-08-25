e57aef440d92d4525efabf057d1d6c0ab7a0bdb2
Sabari S <incrediblesabari02@gmail.com>
2025-08-25 18:54:46 +0530

    add portfolio and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+import Home from './pages/Home.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      Hello World! <br />
-      Site is under construction. <br />
-      Please check back later. <br />
-      In the meantime, know more about me on <a href="https://www.github.com/sabzdotpy">GitHub</a> <br />
-      :) <br />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return (
+        <>
+            Hello World! <br />
+            Site is under construction. <br />
+            Please check back later. <br />
+            In the meantime, know more about me on <a href="https://www.github.com/sabzdotpy">GitHub</a> <br />
+            :) <br />
+
+            View my portfolio <a href="/portfolio.pdf">here</a>.
+        </>
+    )
+}

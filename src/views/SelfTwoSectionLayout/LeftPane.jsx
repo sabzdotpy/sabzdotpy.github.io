e9553554d67380b83db8bd3c86e93812e403ba7f
Sabari S <incrediblesabari02@gmail.com>
2025-10-20 17:16:38 +0530

    add spinning animation and image container styles to LeftPane

--- src/views/SelfTwoSectionLayout/LeftPane.jsx
+++ src/views/SelfTwoSectionLayout/LeftPane.jsx
@@ -1,28 +1,27 @@
 import logo from "../../assets/sabz-dark-420x420-60kb.png";
+import "./SelfTwoSectionLayout.scss";
 
 import { IoLocationOutline, IoLogoLinkedin } from "react-icons/io5";
 import { CiMail } from "react-icons/ci";
-import { MdOutlineLocalPhone } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
-
 
 import CLink from "../../components/CLink";
 import { Link } from "react-router-dom";
 
+
 export default function LeftPane({ className }) {
+  const greetings = [
+    "Hello! 👋",
+    "Hey! 👋",
+    "Welcome! 🙏",
+    "Hi there! 👋",
+    "Wazzuup! 😎",
+  ];
 
-    const greetings = [
-        "Hello! 👋",
-        "Hey! 👋",
-        "Welcome! 🙏",
-        "Hi there! 👋",
-        "Wazzuup! 😎",
-    ]
-
-    return (
-      <>
-        <div
-          className={`
+  return (
+    <>
+      <div
+        className={`
             leftPaneContent 
             ${className} 
             h-full 
@@ -39,51 +38,64 @@ export default function LeftPane({ className }) {
             border-(--color-primary)/25
             mt-6
         `}
-        >
-          <h4 className="mb-6">{greetings[Math.floor(Math.random() * greetings.length)]}</h4>
+      >
+        <h4 className="mb-6">
+          {greetings[Math.floor(Math.random() * greetings.length)]}
+        </h4>
 
-          <img 
-            src={logo} 
+        <div className="imageContainer relative h-36 w-36 rounded-full flex justify-center items-center overflow-hidden">
+          <img
+            src={logo}
             alt="Headshot"
-            className="h-36 w-36 rounded-full border-2 border-dashed border-(--color-primary) object-cover" 
+            className="h-36 w-36 rounded-full object-cover relative z-10"
           />
-          <h2 className="m-0 mt-6 mb-0 text-(--color-primary) font-[Playfair_Display]">Sabari S</h2>
-          <h5 className="m-0 font-semibold! w-3/4 text-[1rem]! text-(--color-text)/80">
-            Backend Developer / Student 
-          </h5>
+        </div>
+        <h2 className="m-0 mt-6 mb-0 text-(--color-primary) font-[Playfair_Display]">
+          Sabari S
+        </h2>
+        <h5 className="m-0 font-semibold! w-3/4 text-[1rem]! text-(--color-text)/80">
+          Backend Developer / Student
+        </h5>
 
-          <div className="divider my-4 h-0.5 w-20 bg-(--color-primary) opacity-20"></div>
+        <div className="divider my-4 h-0.5 w-20 bg-(--color-primary) opacity-20"></div>
 
-          <div className="links flex flex-col gap-2">
-            <div>
-                <IoLocationOutline className="inline mb-1 mr-1" />
-                <span>Chennai, India</span>
-            </div>
-            <div>
-                <CiMail className="inline mb-1 mr-1" />
-                <CLink
-                    to={"mailto:sabari.codes@gmail.com"}
-                    className={"text-(--color-text)!"}
-                >
-                    sabari.codes@gmail.com
-                </CLink>
-            </div>
-            {/* <div>
+        <div className="links flex flex-col gap-2">
+          <div>
+            <IoLocationOutline className="inline mb-1 mr-1" />
+            <span>Chennai, India</span>
+          </div>
+          <div>
+            <CiMail className="inline mb-1 mr-1" />
+            <CLink
+              to={"mailto:sabari.codes@gmail.com"}
+              className={"text-(--color-text)!"}
+            >
+              sabari.codes@gmail.com
+            </CLink>
+          </div>
+          {/* <div>
                 <MdOutlineLocalPhone className="inline mb-1 mr-1" />
                 <CLink className={"text-(--color-text)!"} to={"tel:+91 8754605197"}>+91 8754605197</CLink>
             </div> */}
-          </div>
-
-          <div className="socials flex gap-2 mt-6 justify-center items-center">
-              <Link className="text-(--color-text)!" to={"https://linkedin.com/in/sabari01"} target="_blank">
-                    <IoLogoLinkedin size={"20px"} />
-              </Link>
-              <Link className="text-(--color-text)!" to={"https://github.com/sabzdotpy"} target="_blank">
-                    <FaGithub size={"20px"} />
-              </Link>
-          </div>
-
         </div>
-      </>
-    );
+
+        <div className="socials flex gap-2 mt-6 justify-center items-center">
+          <Link
+            className="text-(--color-text)!"
+            to={"https://linkedin.com/in/sabari01"}
+            target="_blank"
+          >
+            <IoLogoLinkedin size={"20px"} />
+          </Link>
+          <Link
+            className="text-(--color-text)!"
+            to={"https://github.com/sabzdotpy"}
+            target="_blank"
+          >
+            <FaGithub size={"20px"} />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
